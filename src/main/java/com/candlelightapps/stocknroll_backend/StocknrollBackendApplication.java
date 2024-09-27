77574ee0d46c77bf0591803823caadcfbbe94e01
Liam O'Hara <liam.ohara@gmail.com>
2024-09-27 12:26:20 +0100

    Initial commit

---
+++ src/main/java/com/candlelightapps/stocknroll_backend/StocknrollBackendApplication.java
@@ -0,0 +1,13 @@
+package com.candlelightapps.stocknroll_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocknrollBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocknrollBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/candlelightapps/stocknroll_backend/StocknrollBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.candlelightapps.stocknroll_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocknrollBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
