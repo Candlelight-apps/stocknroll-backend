f030baeb3bcd4840f5a3ea55cba4b4943f63d90d
Liam O'Hara <liam.ohara@gmail.com>
2024-09-30 11:44:39 +0100

    Feat: implement getAllIngredients method in IngredientManagerServiceImpl.

--- src/main/java/com/candlelightapps/stocknroll_backend/service/IngredientManagerServiceImpl.java
+++ src/main/java/com/candlelightapps/stocknroll_backend/service/IngredientManagerServiceImpl.java
@@ -1,4 +1,15 @@
 package com.candlelightapps.stocknroll_backend.service;
 
-public class IngredientManagerServiceImpl {
+import com.candlelightapps.stocknroll_backend.model.Ingredient;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class IngredientManagerServiceImpl implements IngredientManagerService {
+    @Override
+    public List<Ingredient> getAllIngredients() {
+        List<Ingredient> ingredientList = null;
+        return ingredientList;
+    }
 }
