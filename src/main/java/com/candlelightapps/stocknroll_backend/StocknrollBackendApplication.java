4f8f555755f32720c6fb51c6e3a1101487087f52
rsheikh <red.sheikh@gmail.com>
2024-09-30 15:40:20 +0100

    fix: modified status code from OK to CREATED

--- src/main/java/com/candlelightapps/stocknroll_backend/StocknrollBackendApplication.java
+++ src/main/java/com/candlelightapps/stocknroll_backend/StocknrollBackendApplication.java
@@ -7,10 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StocknrollBackendApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(StocknrollBackendApplication.class, args);
-
-
 	}
 
 }
