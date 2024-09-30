89f4cf641679896c37fe97d2641fa08771dba7e0
rsheikh <red.sheikh@gmail.com>
2024-09-30 13:59:46 +0100

    feat: Implement addRecipe endpoint

--- src/main/java/com/candlelightapps/stocknroll_backend/service/RecipeManagerService.java
+++ src/main/java/com/candlelightapps/stocknroll_backend/service/RecipeManagerService.java
@@ -1,4 +1,8 @@
 package com.candlelightapps.stocknroll_backend.service;
 
+import com.candlelightapps.stocknroll_backend.model.Recipe;
+
 public interface RecipeManagerService {
+
+    Recipe addRecipe(Recipe recipe);
 }
--- src/main/java/com/candlelightapps/stocknroll_backend/service/RecipeManagerServiceImpl.java
+++ src/main/java/com/candlelightapps/stocknroll_backend/service/RecipeManagerServiceImpl.java
@@ -1,5 +1,6 @@
 package com.candlelightapps.stocknroll_backend.service;
 
+import com.candlelightapps.stocknroll_backend.model.Recipe;
 import com.candlelightapps.stocknroll_backend.repository.RecipeManagerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,9 @@ public class RecipeManagerServiceImpl implements RecipeManagerService {
 
     @Autowired
     RecipeManagerRepository recipeManagerRepository;
+
+    @Override
+    public Recipe addRecipe(Recipe recipe) {
+        return recipeManagerRepository.save(recipe);
+    }
 }
