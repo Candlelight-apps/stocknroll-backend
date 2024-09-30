482afe7c3ed2c44d32df270727dae6e1b1f24787
rsheikh <red.sheikh@gmail.com>
2024-09-30 11:50:12 +0100

    feat: implement interfaces in service classes

--- src/main/java/com/candlelightapps/stocknroll_backend/service/RecipeManagerServiceImpl.java
+++ src/main/java/com/candlelightapps/stocknroll_backend/service/RecipeManagerServiceImpl.java
@@ -1,4 +1,12 @@
 package com.candlelightapps.stocknroll_backend.service;
 
-public class RecipeManagerServiceImpl {
+import com.candlelightapps.stocknroll_backend.repository.RecipeManagerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RecipeManagerServiceImpl implements RecipeManagerService {
+
+    @Autowired
+    RecipeManagerRepository recipeManagerRepository;
 }
