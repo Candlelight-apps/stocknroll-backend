dd8daf18121b2dd7f54a5048fa31305223ee394e
Liam O'Hara <liam.ohara@gmail.com>
2024-09-30 14:21:56 +0100

    Feat: add test for getAllIngredients when ingredient respository is populated and implementation.
    Test: added test for getAllIngredients method in IngredientManagerSeviceImpl when ingredients repository is populated.
    
    Feat: implement logic in getAllIngredients method in IngredientManagerServiceImpl to pass test.

--- src/main/java/com/candlelightapps/stocknroll_backend/service/IngredientManagerServiceImpl.java
+++ src/main/java/com/candlelightapps/stocknroll_backend/service/IngredientManagerServiceImpl.java
@@ -1,15 +1,23 @@
 package com.candlelightapps.stocknroll_backend.service;
 
 import com.candlelightapps.stocknroll_backend.model.Ingredient;
+import com.candlelightapps.stocknroll_backend.repository.IngredientManagerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class IngredientManagerServiceImpl implements IngredientManagerService {
+
+    @Autowired
+    IngredientManagerRepository ingredientRepository;
+
     @Override
     public List<Ingredient> getAllIngredients() {
-        List<Ingredient> ingredientList = null;
+        List<Ingredient> ingredientList = new ArrayList<>();
+        ingredientRepository.findAll().forEach(ingredientList::add);
         return ingredientList;
     }
 }
--- src/test/java/com/candlelightapps/stocknroll_backend/service/IngredientManagerServiceImplTest.java
+++ src/test/java/com/candlelightapps/stocknroll_backend/service/IngredientManagerServiceImplTest.java
@@ -2,12 +2,15 @@ package com.candlelightapps.stocknroll_backend.service;
 
 import com.candlelightapps.stocknroll_backend.model.Ingredient;
 import com.candlelightapps.stocknroll_backend.repository.IngredientManagerRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,7 +25,39 @@ class IngredientManagerServiceImplTest {
     @InjectMocks
     private IngredientManagerServiceImpl ingredientManagerServiceImpl;
 
-    List<Ingredient> ingredientList;
+    List<Ingredient> ingredientList = new ArrayList<>();
+    Ingredient canOfTomatoes;
+    Ingredient cornflakes;
+    Ingredient bread;
+
+    @BeforeEach
+    public void setup() {
+
+        canOfTomatoes = Ingredient.builder()
+                .id(1L)
+                .name("Can of tomatoes")
+                .category("Vegetables")
+                .quantity(4)
+                .expiryDate(LocalDate.of(2025,12,31))
+                .build();
+
+        cornflakes = Ingredient.builder()
+                .id(2L)
+                .name("Cornflakes")
+                .category("Breakfast cereals")
+                .quantity(1)
+                .expiryDate(LocalDate.of(2025,5,31))
+                .build();
+
+        bread = Ingredient.builder()
+                .id(3L)
+                .name("Wholemeal bread")
+                .category("Bread")
+                .quantity(1)
+                .expiryDate(LocalDate.of(2024,10,11))
+                .build();
+
+    }
 
     @Test
     @DisplayName("Returns null list of Ingredients when ingredient repository is empty.")
@@ -35,5 +70,21 @@ class IngredientManagerServiceImplTest {
         List<Ingredient> actualResults = ingredientManagerServiceImpl.getAllIngredients();
 
         assertEquals(ingredientList, actualResults);
+    }
+
+    @Test
+    @DisplayName("Returns list of all Ingredients from ingredient repository when repository is populated")
+    void testGetAllIngredients_whenIngredientRepositoryIsPopulated() {
+
+        ingredientList.add(canOfTomatoes);
+        ingredientList.add(cornflakes);
+        ingredientList.add(bread);
+
+        when(mockIngredientRepository.findAll()).thenReturn(ingredientList);
+
+        List<Ingredient> actualResults = ingredientManagerServiceImpl.getAllIngredients();
+
+        assertEquals(ingredientList, actualResults);
+
     }
 }
