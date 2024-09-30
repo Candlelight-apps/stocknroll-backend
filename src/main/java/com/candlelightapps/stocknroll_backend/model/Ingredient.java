471100848dda65b19a2e106c935fc60183307ceb
Liam O'Hara <liam.ohara@gmail.com>
2024-09-30 12:32:49 +0100

    Feat: add body of Ingredient model class aligned with main brain.

--- src/main/java/com/candlelightapps/stocknroll_backend/model/Ingredient.java
+++ src/main/java/com/candlelightapps/stocknroll_backend/model/Ingredient.java
@@ -1,4 +1,38 @@
 package com.candlelightapps.stocknroll_backend.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false, nullable = false)
+    Long id;
+
+    @Column
+    String name;
+
+    @Column
+    String category;
+
+    @Column
+    int quantity;
+
+    @Column
+    LocalDate expiryDate;
+
+    @Column
+    String imageUrl; //nice to have
+
 }
