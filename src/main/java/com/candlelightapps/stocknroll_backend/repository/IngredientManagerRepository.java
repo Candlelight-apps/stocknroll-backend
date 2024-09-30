2ac0b45f815cde78892c7b8a94bbd7ba0f15de22
rsheikh <red.sheikh@gmail.com>
2024-09-30 11:45:06 +0100

    feat: implement extension of CrudRepository in repository classes

--- src/main/java/com/candlelightapps/stocknroll_backend/repository/IngredientManagerRepository.java
+++ src/main/java/com/candlelightapps/stocknroll_backend/repository/IngredientManagerRepository.java
@@ -1,4 +1,8 @@
 package com.candlelightapps.stocknroll_backend.repository;
 
-public interface IngredientManagerRepository {
+import com.candlelightapps.stocknroll_backend.model.Ingredient;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IngredientManagerRepository extends CrudRepository<Ingredient, Long> {
+
 }
