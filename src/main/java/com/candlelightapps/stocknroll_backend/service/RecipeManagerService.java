91af2856d35a44935e39c059d26266e143dc38ab
rsheikh <red.sheikh@gmail.com>
2024-10-01 14:34:13 +0100

    feat: implement getRecipesByCriteria endpoint

--- src/main/java/com/candlelightapps/stocknroll_backend/service/RecipeManagerService.java
+++ src/main/java/com/candlelightapps/stocknroll_backend/service/RecipeManagerService.java
@@ -1,8 +1,13 @@
 package com.candlelightapps.stocknroll_backend.service;
 
 import com.candlelightapps.stocknroll_backend.model.Recipe;
+import com.candlelightapps.stocknroll_backend.model.spoonacular.recipeByCriteria.Result;
+
+import java.util.ArrayList;
 
 public interface RecipeManagerService {
 
     Recipe addRecipe(Recipe recipe);
+
+    ArrayList<Result> getRecipeByCriteria(String cuisine, String diet, String intolerance);
 }
--- src/main/java/com/candlelightapps/stocknroll_backend/service/RecipeManagerServiceImpl.java
+++ src/main/java/com/candlelightapps/stocknroll_backend/service/RecipeManagerServiceImpl.java
@@ -1,9 +1,15 @@
 package com.candlelightapps.stocknroll_backend.service;
 
+import com.candlelightapps.stocknroll_backend.exception.ItemNotFoundException;
 import com.candlelightapps.stocknroll_backend.model.Recipe;
+import com.candlelightapps.stocknroll_backend.model.spoonacular.recipeByCriteria.Data;
+import com.candlelightapps.stocknroll_backend.model.spoonacular.recipeByCriteria.Result;
 import com.candlelightapps.stocknroll_backend.repository.RecipeManagerRepository;
+import com.candlelightapps.stocknroll_backend.service.SpoonacularApi.SpoonacularDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 public class RecipeManagerServiceImpl implements RecipeManagerService {
@@ -14,5 +20,16 @@ public class RecipeManagerServiceImpl implements RecipeManagerService {
     @Override
     public Recipe addRecipe(Recipe recipe) {
         return recipeManagerRepository.save(recipe);
+    }
+
+    @Override
+    public ArrayList<Result> getRecipeByCriteria(String cuisine, String diet, String intolerance) {
+        Data recipeData  = SpoonacularDAO.getRecipesByCriteria(cuisine, diet, intolerance);
+
+        if(!recipeData.results().isEmpty()) {
+            return recipeData.results();
+        } else {
+            throw new ItemNotFoundException("No recipes found matching criteria.");
+        }
     }
 }
