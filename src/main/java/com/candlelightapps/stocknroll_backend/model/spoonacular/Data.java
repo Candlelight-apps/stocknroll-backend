157f82913b7e34957d75cf68d57510462cb77758
princyjp <princy.philip@outlook.com>
2024-10-01 12:40:31 +0100

    Spoonacular:Data: remove all fields except results

--- src/main/java/com/candlelightapps/stocknroll_backend/model/spoonacular/Data.java
+++ src/main/java/com/candlelightapps/stocknroll_backend/model/spoonacular/Data.java
@@ -2,5 +2,5 @@ package com.candlelightapps.stocknroll_backend.model.spoonacular;
 
 import java.util.ArrayList;
 
-public record Data(ArrayList<Result>results, int offset, int number, int totalResults) {
+public record Data(ArrayList<Result>results) {
 }
