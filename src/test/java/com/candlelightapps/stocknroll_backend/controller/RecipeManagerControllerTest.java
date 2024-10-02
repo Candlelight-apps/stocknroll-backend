bfd2fecb8a598d6518ca9991fd745a722f3ee4a3
princyjp <princy.philip@outlook.com>
2024-10-02 18:43:59 +0100

    test:RecipeManagerControllerTest:getRecipesByIngredient:Return 404 NOT_FOUND for null input

--- src/test/java/com/candlelightapps/stocknroll_backend/controller/RecipeManagerControllerTest.java
+++ src/test/java/com/candlelightapps/stocknroll_backend/controller/RecipeManagerControllerTest.java
@@ -221,4 +221,28 @@ class RecipeManagerControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$[1].id").value(665496L))
                 .andExpect(MockMvcResultMatchers.jsonPath("$[1].title").value("Yakitori Glaze"));
     }
+    @Test
+    @DisplayName("Return 404 NOT_FOUND for null input")
+    void testGetRecipesByIngredient_nullInput() throws Exception {
+        ArrayList<Result> recipeAL = new ArrayList<>();
+        recipeAL.add(japaneseVeganGluten);
+        recipeAL.add(japaneseVeganGluten2);
+
+        ArrayList<String> ingredientsList = new ArrayList<>();
+        ingredientsList.add("Beef");
+        ingredientsList.add("egg");
+        ingredientsList.add("tomatoes");
+        String ingredients = null;
+
+        when(mockRecipeManagerServiceImpl.getRecipesByIngredient(ingredientsList)).thenReturn(recipeAL);
+        this.mockMvcController.perform(MockMvcRequestBuilders
+                        .get("/api/v1/stocknroll/recipes/ingredient?")
+                        .param("values",ingredients)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].title").doesNotExist());
+
+    }
 }
